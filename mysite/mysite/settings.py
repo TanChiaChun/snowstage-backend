d3adc751c5453bc6713af883706f1e296e473ef5
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2023-07-02 16:02:12 +0800

    Test GitHub Actions on exit after error on 1 step

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v*jfy2yl^o03r9-kv4v()phg#nvc8vkjx_70^3r!7^!@nim#u'
+SECRET_KEY = 'django-insecure-v*jfy2yl^o03r9-kv4v()phg#nvc8vkjx_70^3r!7^!@nim#ua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
