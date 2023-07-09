b2c099ccece85672803b367abf152bbc39e03d94
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2023-07-09 09:17:42 +0800

    Remove unittest failing test case

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,5 @@
+"""Testing"""
 import unittest
 
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FO')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
